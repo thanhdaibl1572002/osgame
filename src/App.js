95a79035e0a81d59d381628fc9b0825f86872375
Trương Thành Ä?ại <thanhdaibl1572002@gmail.com>
2023-03-03 19:09:07 +0700

    update content

--- src/App.js
+++ src/App.js
@@ -16,11 +16,12 @@ const App = () => {
      <div className="layout-container">
         <Header />
         <Routes>
-          <Route path='/home' element={<Home/>} />
-          <Route path='/install' element={<Install/>} />
-          <Route path='/team' element={<Team/>} />
-          <Route path='/contact' element={<Contact/>} />
+          <Route path='/osgame/home' element={<Home/>} />
+          <Route path='/osgame/install' element={<Install/>} />
+          <Route path='/osgame/team' element={<Team/>} />
+          <Route path='/osgame/contact' element={<Contact/>} />
           <Route path='/' element={<Home/>} />
+          <Route path='/osgame' element={<Home/>} />
         </Routes>
         <Footer />
       </div>
